c988bbf67d61cbe02c8d3137758ad95228ef7e3d
Md. Asif Iqbal Fahim <33654834+AIFahim@users.noreply.github.com>
2021-12-12 07:32:37 +0600

    Codeforces Round #642 (Div. 3) A. Most Unstable Array

---
+++ Codeforces Round #642 (Div. 3) - A. Most Unstable Array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+
+int main(int argc, char **argv)
+{
+    int tc;
+    while(tc--)
+    {
+          int x;
+//        3 100
+//        50 0 50
+//
+//        2 100
+//        100 0
+//
+//        6 100
+//        50 0 30 0 20 0   = (50 -0) + (0 - 30 ) + (30 - 0) + (0 - 20)
+//
+//        4 100
+//        50 0 50 0
+        long long n , K ,tmpK;
+        cin>>n>>m;
+        vector < int > vi(n , 0);
+        while( k != 0 ){
+            tmpK = floor(tmpK / 2) + 1;
+            for(int i = 0; i < n ; i+=2)
+            {
+
+            }
+        }
+
+    }
+    return 0;
+
+}
