ab81f75c27ad5cad4cdab0d4db6fae1b91204959
Md. Asif Iqbal Fahim <33654834+AIFahim@users.noreply.github.com>
2021-12-12 07:33:17 +0600

    Cf 630

---
+++ A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    int a, b ,c,d;
+    cin>>tc;
+    while(tc--)
+    {
+
+        cout<<"Yes";<<endl;
+        cout<<"No"<<end;
+
+    }
+
+
+}
